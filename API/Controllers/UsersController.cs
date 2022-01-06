dfb2f920d4b4be9b6577011c73c72c6d22017e76
Alan, Barnes <alanbarnes447@gmail.com>
2022-01-05 19:38:06 -0600

    End of Chapter 8

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -3,42 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.DTOs;
 using API.Entities;
+using API.Interfaces;
+using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
+    [Authorize] 
 
     public class UsersController : BaseApiController
     {
-        private readonly DataContext _context;
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
 
-        public UsersController(DataContext context)
+        public UsersController(IUserRepository userRepository, IMapper mapper)
         {
-            _context = context;
+            _mapper = mapper;
+            _userRepository = userRepository;
         }
 
         //Endpoint to get to all users in the database
         //Endpoint to get to a specific user in the database
-        [AllowAnonymous]
+        
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() //public ActionResult<IEnumerable<AppUser>> GetUsers() //public ActionResult<List<AppUser>> GetUsers() 
+        public async Task<ActionResult<IEnumerable<MemberDtos>>> GetUsers() //public ActionResult<IEnumerable<AppUser>> GetUsers() //public ActionResult<List<AppUser>> GetUsers() 
         {
-            //var users = _context.Users.ToList(); //.ToList() get users into the system as a list
-            //return users;
-            return await _context.Users.ToListAsync();
+            var users = await _userRepository.GetMembersAsync();
+            
+            return Ok(users);
         }
-
-        // api/users/3
-        [Authorize]
+        
         [HttpGet("{id}")]
-        public async Task<ActionResult<AppUser>> GetUser(int id)
+        public async Task<ActionResult<MemberDtos>> GetUser(string username)
         {
-            //var user = _context.Users.Find(id);
-            //return user;
-            return await _context.Users.FindAsync(id);
+            return await _userRepository.GetMemberAsync(username);
         }
     }
 }
